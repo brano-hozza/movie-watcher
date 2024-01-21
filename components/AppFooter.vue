e6d60c8e4dc60f24520e67f7ca2fe1f18eec5108
Branislav Hozza <brankohozza@gmail.com>
2024-01-21 12:24:04 +0100

    init

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-10 flex">
+    <div class="flex-1"></div>
+    <div class="flex-1">Branislav Hozza</div>
+    <div class="flex-1"></div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full h-20">
+    <ul class="flex gap-2">
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/contact">Contact</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
